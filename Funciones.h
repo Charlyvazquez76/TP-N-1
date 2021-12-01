f7f37f24af3f5cdea1b8e081eec2710188951995
Charlyvazquez76 <95045258+Charlyvazquez76@users.noreply.github.com>
2021-11-30 21:49:19 -0300

    Update Funciones.h

--- Funciones.h
+++ Funciones.h
@@ -1,1 +1,11 @@
 
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+void suma(int num1 , int num2);
+void resta(int num1 , int num2);
+void multiplicacion(int num1 , int num2);
+void division(float num1 , float num2);
+void factorial(int num1);
+int validar_numero(char numero[]);
+#endif // FUNCIONES_H_INCLUDED
